2097125bd19a7d43eb57d794ac8f0376b225ad18
Santosh Kumar <santosh.achary0706@gmail.com>
2022-05-09 14:54:40 +0530

    Add files via upload

---
+++ ROBO.ino
@@ -0,0 +1,107 @@
+int m1a = 9;
+int m1b = 10;
+int m2a = 11;
+int m2b = 12;
+int buzz = 4;
+int bled = 5;
+char val;
+
+void setup() 
+{  
+pinMode(m1a, OUTPUT);  // Digital pin 10 set as output Pin
+pinMode(m1b, OUTPUT);  // Digital pin 11 set as output Pin
+pinMode(m2a, OUTPUT);  // Digital pin 12 set as output Pin
+pinMode(m2b, OUTPUT);  // Digital pin 13 set as output Pin
+Serial.begin(9600);
+}
+
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+  val = Serial.read();
+  Serial.println(val);
+  }
+
+  if( val == 'F') // Forward
+    {
+      digitalWrite(m1a, HIGH);
+      digitalWrite(m1b, LOW);
+      digitalWrite(m2a, HIGH);
+      digitalWrite(m2b, LOW);  
+    }
+  else if(val == 'B') // Backward
+    {
+      digitalWrite(m1a, LOW);
+      digitalWrite(m1b, HIGH);
+      digitalWrite(m2a, LOW);
+      digitalWrite(m2b, HIGH); 
+    }
+  
+    else if(val == 'L') //Left
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, HIGH);
+    digitalWrite(m2b, LOW);
+    }
+    else if(val == 'R') //Right
+    {
+    digitalWrite(m1a, HIGH);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW); 
+    }
+    
+  else if(val == 'S') //Stop
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW); 
+    }
+  else if(val == 'I') //Forward Right
+    {
+    digitalWrite(m1a, HIGH);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW);
+    }
+  else if(val == 'J') //Backward Right
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, HIGH);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, LOW);
+    }
+   else if(val == 'G') //Forward Left
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, HIGH);
+    digitalWrite(m2b, LOW);
+    }
+  else if(val == 'H') //Backward Left
+    {
+    digitalWrite(m1a, LOW);
+    digitalWrite(m1b, LOW);
+    digitalWrite(m2a, LOW);
+    digitalWrite(m2b, HIGH); 
+    }
+  else if(val == 'V')  
+    {
+      digitalWrite(buzz,HIGH);
+    }
+  else if(val == 'v')  
+    {
+      digitalWrite(buzz,LOW);
+    }  
+  else if(val == 'U')  
+    {
+      digitalWrite(bled,HIGH);
+    }  
+  else if(val == 'u')  
+    {
+      digitalWrite(buzz,LOW);
+    }  
+}
